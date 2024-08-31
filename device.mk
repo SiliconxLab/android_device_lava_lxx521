add24c4c880e5d5d3a4e8fb8d803938187f9734c
Jerry Zhang <zhangjerry@google.com>
2024-08-31 07:46:02 +0200

    lxx521: Add recovery init
    The kernel does not support aio with ffs.
    Set the property so adb/mtp know to use the compatible
    functions, and the same while in recovery.
    Set usb controller.
    
    Bug: 37916658
    Test: Use adb / use adb in recovery
    Change-Id: Id35a05fcb9952dcf7239bd15a61cc620537419bc

--- device.mk
+++ device.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES += \
     init.connectivity.common.rc \
     init.modem.rc \
     init.mt6878.rc \
+    init.recovery.mt6878.rc \
     init.mt6878.usb.rc \
     init.mtkgki.rc \
     init.project.rc \
