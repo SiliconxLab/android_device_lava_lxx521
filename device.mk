24b78ce6f11d4045a57dae7084f78a15bb008cf4
suchit-7x <gtsuchit7x@gmail.com>
2026-09-18 08:11:24 +0530

    lxx521: Import minimalized init from LXX521_SW_V13_HW_V1.0_20260811

--- device.mk
+++ device.mk
@@ -61,6 +61,26 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.mt6878 \
+    fstab.mt6878.vendor_ramdisk \
+    init_conninfra.rc \
+    init.aee.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.modem.rc \
+    init.mt6878.rc \
+    init.mt6878.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    ueventd.mt6878.rc
+
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
+    init.insmod.mt6878.cfg
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
